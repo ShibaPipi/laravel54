df1b684915d29bf6812f0f9346b7ca26d7022dca
ShibaPipi <906262260@qq.com>
2018-07-24 10:31:54 +0800

    为了安全考虑，暂时将轮播图置于服务器本地

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -12,15 +12,15 @@
                     </ol><!-- Wrapper for slides -->
                     <div class="carousel-inner">
                         <div class="item active">
-                            <img src="http://ww1.sinaimg.cn/large/44287191gw1excbq6tb3rj21400migrz.jpg" alt="..." />
+                            <img src="images/44287191gw1excbq6tb3rj21400migrz.jpg" alt="..." />
                             <div class="carousel-caption">男人永远不会长大，只会变老</div>
                         </div>
                         <div class="item">
-                            <img src="http://ww3.sinaimg.cn/large/44287191gw1excbq5iwm6j21400min3o.jpg" alt="..." />
+                            <img src="images/44287191gw1excbq5iwm6j21400min3o.jpg" alt="..." />
                             <div class="carousel-caption">欢迎来到简书</div>
                         </div>
                         <div class="item">
-                            <img src="http://ww2.sinaimg.cn/large/44287191gw1excbq4kx57j21400migs4.jpg" alt="..." />
+                            <img src="images/44287191gw1excbq4kx57j21400migs4.jpg" alt="..." />
                             <div class="carousel-caption">早上好！</div>
                         </div>
                     </div>
